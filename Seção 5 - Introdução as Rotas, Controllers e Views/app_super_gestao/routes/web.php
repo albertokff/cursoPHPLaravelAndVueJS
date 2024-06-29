f39dba51a0289b3fe329eb5e9be87d6f8207f4ba
Diogo  Alberto <106744072+albertokff@users.noreply.github.com>
2024-06-29 14:07:04 -0300

    ✨feat: Seção 5 - Introdução a Rotas, Controllers e Views

--- Seção 5 - Introdução as Rotas, Controllers e Views/app_super_gestao/routes/web.php
+++ Seção 5 - Introdução as Rotas, Controllers e Views/app_super_gestao/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return 'Olá, seja bem-vindo ao curso!';
 });
@@ -9,7 +10,14 @@ Route::get('/', function () {
 Route::get('/about', function() {
     return 'Sobre nós';
 });
-
+ 
 Route::get('/contact', function() {
     return 'Contato';
 });
+*/
+
+Route::get('/', 'App\Http\Controllers\PrincipalController@principal');
+
+Route::get('/about', 'App\Http\Controllers\SobreNosController@sobreNos');
+
+Route::get('/contact', 'App\Http\Controllers\ContatoController@contato');
