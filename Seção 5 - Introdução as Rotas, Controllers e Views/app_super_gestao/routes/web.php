8c02b66503c67c9ae150223fc3bac9b1eb6a65ba
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-07-13 16:52:12 -0300

    ✨feat: Seção 6 concluída

--- Seção 5 - Introdução as Rotas, Controllers e Views/app_super_gestao/routes/web.php
+++ Seção 5 - Introdução as Rotas, Controllers e Views/app_super_gestao/routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', 'App\Http\Controllers\PrincipalController@principal');
 Route::get('/about', 'App\Http\Controllers\SobreNosController@sobreNos');
 
 Route::get('/contact', 'App\Http\Controllers\ContatoController@contato');
+
+Route::get('/contato/{nome}/{categoria_id}', //
+    function(
+        string $nome = 'Desconhecido', 
+        int $categoria_id = 1
+    ) {
+        echo "Estamos aqui: $nome - $categoria_id";
+
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
